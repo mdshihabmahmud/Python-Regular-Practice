41bea7aeac926a12df122566cb1fbb4054b87cb6
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-07 14:55:49 +0600

    Create Varriable.py

---
+++ Varriable.py
@@ -0,0 +1,14 @@
+a = 5
+print(a)
+b = "Md. Shihab Mahmud"
+print(b)
+x = str(9)
+print(x)
+y = float(9)
+print(y)
+var1 = 6
+var2 = 8
+var3 = var1+var2
+print(var3)
+myvar = "Md. Shihab Mahmud"
+print("My Name is "+ myvar)
