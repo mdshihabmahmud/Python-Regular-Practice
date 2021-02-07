40b3780279aaee807eb324b04a94f07813a90dc1
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-07 14:56:29 +0600

    Create Comments.py

---
+++ Comments.py
@@ -0,0 +1,12 @@
+#My Name is Md. Shihab Mahmud, it is single lime comment
+print("Python Comments")
+"""
+This is a
+multiline 
+comments
+"""
+print("Md. Shihab Mahmud")
+#shihab
+#mahmud
+#diucse
+print("Daffodil International University")
