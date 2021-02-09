b1875f835bf1fb46f5cd3a4efd68973dd000d07a
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-09 10:10:12 +0600

    Create PyrhonString.py

---
+++ PyrhonString.py
@@ -0,0 +1,18 @@
+#String
+VAR = "Md. Shihab Mahmud"
+print(VAR)
+#Python - Slicing Strings
+b = "Md. Shihab Mahmud"
+print(b[2:5])
+#Python - Modify Strings
+text = "shihab"
+print(text.upper())#UpperCase
+text1 = "SHIHAB"
+print(text1.lower())#LowerCase
+a = "   Hello, World! "  #Remove Whitespace
+print(a.strip())
+#Python - String Concatenation
+var1 = "Md. Shihab"
+var2 = "Mahmud"
+var = var1 + " " + var2
+print(var)
