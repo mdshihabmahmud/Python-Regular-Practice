9f4c7898753731955e409a4fd426487a3162a931
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-10 22:44:52 +0600

    Create PythonBooleans.py

---
+++ PythonBooleans.py
@@ -0,0 +1,16 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+m = "DIUCSE"
+n = 60
+print(bool(m))
+print(bool(n))
+#Some Values are always false
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
