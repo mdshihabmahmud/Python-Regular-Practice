778b7e8a2dfa24413781bb0459916920329b594c
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-10 22:45:25 +0600

    Create PythonOperators.py

---
+++ PythonOperators.py
@@ -0,0 +1,18 @@
+#Python Arithmetic Operators
+x = 5
+y = 3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+#Python Assignment Operators
+x = 5
+x += 3
+print(x)
+
+x = 5
+x *= 3
+print(x)
