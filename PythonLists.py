9a96dd4668a3821ddb0b36ef27f84f158d3287fe
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-11 19:32:32 +0600

    Create PythonLists.py

---
+++ PythonLists.py
@@ -0,0 +1,9 @@
+ListA = ["apple", "banana", "cherry"]
+print(ListA)
+ListB = ["apple", "banana", "cherry"]
+print(ListB[1])
+ListC = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(ListC[2:5])
+ListD = ["apple", "banana", "cherry"]
+List1 = ListD.copy()
+print(List1)
