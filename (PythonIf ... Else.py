49147247f09cf278e6776c102652f52e902e6a5c
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-14 11:48:20 +0600

    Create (PythonIf ... Else.py

---
+++ (PythonIf ... Else.py
@@ -0,0 +1,18 @@
+a = 635
+b = 5000
+if b > a:
+ print("b is greater than a")
+x = 33
+y = 33
+if y > x:
+  print("y is greater than x")
+elif x == y:
+  print("x and y are equal")
+  a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
